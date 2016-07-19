b65a7c374d2147a1508ab6f162380f738084c427
Guilherme Dias <guilhermehenriquedias@hotmail.com>
2016-07-18 21:33:42 -0300

    side menu e tabs iniciadas

--- app/app.ts
+++ app/app.ts
@@ -1,25 +1,50 @@
-import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
+import {Component,ViewChild} from '@angular/core';
+import {Platform,MenuController,App, ionicBootstrap,Nav} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
+import {HomePage} from './pages/home/home';
+import {HelloIonicPage} from './pages/hello-ionic/hello-ionic';
+import {ListPage} from './pages/list/list';
 import {TabsPage} from './pages/tabs/tabs';
+import {LoginPage} from './pages/login/login';
 
 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+  templateUrl: './build/app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+  rootPage: any = TabsPage;
+  pages: Array<{title: string, component: any}>;
 
-  private rootPage:any;
+   constructor(
+     private platform: Platform,
+     private menu: MenuController
+   ) {
+     this.initializeApp();
 
-  constructor(private platform:Platform) {
-    this.rootPage = TabsPage;
+     // set our app's pages
+     this.pages = [
+       { title: 'Home', component: HelloIonicPage },
+       { title: 'List Example', component: ListPage },
+       { title: 'Login', component: LoginPage },
+       { title: 'Tabs', component: TabsPage }
+     ];
+   }
 
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-    });
-  }
+   initializeApp() {
+     this.platform.ready().then(() => {
+       // Okay, so the platform is ready and our plugins are available.
+       // Here you can do any higher level native things you might need.
+       StatusBar.styleDefault();
+     });
+   }
+
+   openPage(page) {
+     // close the menu when clicking a link from the menu
+     this.menu.close();
+     // navigate to the new page if it is not the current page
+     this.nav.setRoot(page.component);
+   }
 }
 
-ionicBootstrap(MyApp)
+ionicBootstrap(MyApp);
--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core'
-import {HomePage} from '../home/home';
+import {HelloIonicPage} from '../hello-ionic/hello-ionic';
+import {LoginPage} from '../login/login';
 import {AboutPage} from '../about/about';
-import {ContactPage} from '../contact/contact';
 
 @Component({
   templateUrl: 'build/pages/tabs/tabs.html'
@@ -15,8 +15,8 @@ export class TabsPage {
   constructor() {
     // this tells the tabs component which Pages
     // should be each tab's root Page
-    this.tab1Root = HomePage;
-    this.tab2Root = AboutPage;
-    this.tab3Root = ContactPage;
+    this.tab1Root = HelloIonicPage;
+    this.tab2Root = LoginPage;
+    this.tab3Root = AboutPage;
   }
 }
